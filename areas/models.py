0d2705c28e02ac1743f3a5e37efc509a2cd3a979
Mehemmed Mustafayev <mohammedmustafayev@gmail.com>
2021-06-11 11:02:38 +0400

    versiya-1.3

--- areas/models.py
+++ areas/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Area(models.Model):
+    title = models.CharField(verbose_name="Basliq:",max_length=35)
+    content = models.TextField(verbose_name="Mezmun:")
+    image = models.ImageField(verbose_name="Sekil:")
+    
+    def __str__(self):
+        return self.title
